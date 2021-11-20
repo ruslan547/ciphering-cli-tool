6debdf3fef0b8012acec2c37901abd34a5b37979
ruslan547 <novik.rv93@gmail.com>
2021-11-20 22:12:41 +0300

    add case without config

--- utils/cli.test.js
+++ utils/cli.test.js
@@ -13,22 +13,23 @@ jest.mock('readline', () => ({
   )
 }));
 
-describe('getArgs error', () => {
-  const mockWrite = jest.spyOn(process.stderr, 'write');
-  const mockExit = jest.spyOn(process, 'exit').mockImplementation(() => { });
+const mockWrite = jest.spyOn(process.stderr, 'write');
+const mockExit = jest.spyOn(process, 'exit').mockImplementation(() => { });
 
-  process.argv.push('node', 'cipher', '-c', 'C1-C1-A-R0', '-c', 'C0');
-
-  beforeEach(() => {
-    getArgs();
-  });
-
-  /**
+/**
    * Input: User passes the same cli argument twice;
    * Result: Error message is shown;
    * e.g. input: node my_caesar_cli -c C1-C1-A-R0 -c C0
    * result: Error: You provided -c argument more than once;
    */
+describe('getArgs the same cli argument twice', () => {
+
+  beforeEach(() => {
+    process.argv.length = 0;
+    process.argv.push('node', 'cipher', '-c', 'C1-C1-A-R0', '-c', 'C0');
+    getArgs();
+  });
+
   it('should call process.stderr.write with error message', () => {
     expect(mockWrite)
       .toHaveBeenCalledWith('Error: You provided -c argument more than once\n');
@@ -40,12 +41,35 @@ describe('getArgs error', () => {
   });
 });
 
-describe('getArgs success', () => {
-  /**
+/**
+ * Input: User doesn't pass -c or --config argument;
+ * Result: Error message is shown;
+ */
+describe('getArgs without config', () => {
+
+  beforeEach(() => {
+    process.argv.length = 0;
+    process.argv.push('node', 'cipher');
+    getArgs();
+  });
+
+  it('should call process.stderr.write with error message', () => {
+    expect(mockWrite)
+      .toHaveBeenCalledWith('error: option -c, --config <value> missing\n');
+  });
+
+  it('should call process.exit with 1', () => {
+    expect(mockExit)
+      .toHaveBeenCalledWith(1);
+  });
+});
+
+/**
    * Input: User passes correct sequence of symbols as argument for --config that matches regular expression;
    * Result: test passed
    * Take cipher usage scenarios from first task description usage examples.
    */
+describe('getArgs success', () => {
   it('should return correct args', () => {
     process.argv.length = 0;
     process.argv.push(
